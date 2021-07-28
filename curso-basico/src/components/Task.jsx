d79282c62a258d994f3ae923ec9d6003a11b8f29
GlaytonVerissimo <glayton.verissimo@hotmail.com>
2021-07-28 13:10:39 -0300

    projeto final terminado

--- curso-basico/src/components/Task.jsx
+++ curso-basico/src/components/Task.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {CgClose, CgInfo} from 'react-icons/cg'
 import "./Task.css"
+import { useHistory } from 'react-router-dom'
 
 
 
 const Task = ({task, handleTaskClick, handleTaskRemove}) => {
+    const history = useHistory()
+    const handleTaskDetailsClick = () => {
+        history.push(`/${task.title}`)
+    }
     
     return(
         <div className="task-container"
@@ -14,8 +20,16 @@ const Task = ({task, handleTaskClick, handleTaskRemove}) => {
         </div>
 
         <div className="buttons-container">
-            <button className="remove-task-button" onClick={() => handleTaskRemove(task.id)}>X</button>
-
+            <button 
+            className="remove-task-button"
+             onClick={() => handleTaskRemove(task.id)}>
+                 <CgClose/>
+             </button>
+             <button 
+            className="see-task-details-button" onClick={handleTaskDetailsClick}>
+                 <CgInfo/>
+             </button>
+            
 
 
         </div>
